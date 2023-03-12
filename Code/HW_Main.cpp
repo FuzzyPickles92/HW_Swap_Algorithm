74de89040eeed05c192523485ad22b657214155d
FuzzyPickles92 <97342270+FuzzyPickles92@users.noreply.github.com>
2023-03-12 07:24:34 -0500

    Completed HW Task.

--- Code/HW_Main.cpp
+++ Code/HW_Main.cpp
@@ -1,22 +1,58 @@
+// DeMario Russell 
+// CIS - 251 C++
+// 03/10/2023
+// HW - Swap Algorithm
+//
+
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-// Variable - START
-//	define variables that are global between the two Variable markers
-
-// Variable - END
-
 // Function/Method Declaration - START
-//	define function/method declaration signatures between the two markers
-
+void bubbleSort(int arr[], int arrSize);
+// define function/method declaration signatures between the two markers
 // Function/Method Declaration - END
 
 int main() {
-	cout << "Hello World!";
+    // get user input for array elements
+    int arrSize;
+    cout << "Enter the number of elements in the array: ";
+    cin >> arrSize;
+
+    int* arr = new int[arrSize];
+    for (int i = 0; i < arrSize; i++) {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    // sort the array numerically using bubble sort
+    bubbleSort(arr, arrSize);
+
+    // print the sorted array
+    cout << "Sorted Array: ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // deallocate the dynamically allocated array
+    delete[] arr;
+
+    return 0;  // indicate successful program execution
 }
 
 // Function/Method Definition - START
-//	define function/method declaration signatures between the two markers
-
+void bubbleSort(int arr[], int arrSize) {
+    for (int i = 0; i < arrSize - 1; i++) {
+        for (int j = 0; j < arrSize - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+// define function/method definitions between the two markers
 // Function/Method Definition - END
