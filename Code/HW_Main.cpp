1133e23b9dbee7cf43bfbbb3994384dda072fb41
FuzzyPickles92 <97342270+FuzzyPickles92@users.noreply.github.com>
2023-03-12 08:09:14 -0500

    Added comments back to code.

--- Code/HW_Main.cpp
+++ Code/HW_Main.cpp
@@ -13,11 +13,12 @@ int temp;
 int numComparisons = 0;
 int numSwaps = 0;
 
-// Function Prototypes
+// Function/Method Declaration - START
 void bubbleSort(int arr[], int size);
-
+// define function/method declaration signatures between the two markers
+// Function/Method Declaration - END
 int main() {
-    int arrSize;
+    int arrSize; // get user input for array elements
     cout << "Enter the number of elements you want to sort: ";
     cin >> arrSize;
 
@@ -26,20 +27,20 @@ int main() {
     for (int i = 0; i < arrSize; i++) {
         cin >> arr[i];
     }
-
+    // sort the array numerically using bubble sort
     bubbleSort(arr, arrSize);
-
+    // print the sorted array
     cout << "Sorted elements: ";
     for (int i = 0; i < arrSize; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
-
+    // deallocate the allocated array
     delete[] arr;
 
-    return 0;
+    return 0; // indicate successful program execution
 }
-
+// Function/Method Definition - START
 void bubbleSort(int arr[], int size) {
     bool swapped = true;
     int j = 0;
@@ -58,7 +59,8 @@ void bubbleSort(int arr[], int size) {
             numComparisons++;
         }
     }
-
+    // define function/method definitions between the two markers
+// Function/Method Definition - END
     cout << "Number of comparisons: " << numComparisons << endl;
     cout << "Number of swaps: " << numSwaps << endl;
 }
